264296d57ab5ba7ef43ee318d6d77164cdf89ab5
Aidar Rakhmatullin <raidar@yandex.ru>
2020-11-04 17:58:02 +0300

    add test model for `SqlExpression`.

---
+++ src/test/java/org/raidar/app/sql/test/model/SqlTestExpression.java
@@ -0,0 +1,31 @@
+package org.raidar.app.sql.test.model;
+
+import org.raidar.app.sql.api.builder.SqlQuery;
+import org.raidar.app.sql.api.provider.SqlParamMapper;
+import org.raidar.app.sql.impl.builder.SqlExpression;
+
+public class SqlTestExpression extends SqlExpression {
+
+    public SqlTestExpression() {
+        super();
+    }
+
+    public SqlTestExpression(SqlParamMapper paramMapper) {
+        super(paramMapper);
+    }
+
+    @Override
+    public SqlExpression literal(String argument) {
+        return super.literal(argument);
+    }
+
+    @Override
+    public SqlExpression with(String expression) {
+        return super.with(expression);
+    }
+
+    @Override
+    public SqlExpression subquery(SqlQuery query) {
+        return super.subquery(query);
+    }
+}
